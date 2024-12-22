88a21651eae96829f2bf5f8174d8648183fa1f19
Görkem <grrarr.1907@gmail.com>
2024-12-22 14:17:59 +0300

    cors

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+var cors = require('cors')
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -9,6 +10,7 @@ var apiRouter= require('./app_api/routes/index');
 
 var app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
